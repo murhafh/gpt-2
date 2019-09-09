e2447630854cb617b6e2ffc3fdc845a36afa0c0c
Murhaf <murhaf@me.com>
2019-09-09 18:26:40 +0100

    add params to model

--- src/api.py
+++ src/api.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from flask_restful import Resource, Api, reqparse
 import json
 
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
 
 from conditional_generation import load_generation_model
@@ -13,7 +15,8 @@ class GenerateText(Resource):
         try:
             input_text = request.json['text']
             print(input_text)
-            generated_text = load_generation_model(model_name="355M", raw_text=input_text)
+            generated_text = load_generation_model(model_name="774M", top_k = 40, raw_text=input_text)
+            generated_text = [item.split("\n\n") for item in generated_text]
             return {'generated_text': generated_text }
 
         except Exception as x:
